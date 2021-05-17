cdc79329ea5acdddc75091ab6f27c684a5d52ba1
vuthanh199826 <vuthanh199825@gmail.com>
2021-05-17 16:12:14 +0700

    done bai2

--- src/bai2/Main.java
+++ src/bai2/Main.java
@@ -7,29 +7,72 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         QuanLySach ql = new QuanLySach();
         int choice;
-        while (true){
+        while (true) {
             display();
             System.out.println("Nhap lua chon vao day");
             choice = sc.nextInt();
-            switch (choice){
+            switch (choice) {
                 case 1:
-                    ql.addTaiLieu();
+                    TaiLieu obj;
+                    System.out.println("Ban muon them loai tai lieu nao ???");
+                    System.out.println("1.Sach  2.Tap chi  3.Bao");
+                    int option = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Nhap ma tai lieu");
+                    int MATAILIEU = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Nhap ten nha xuat ban");
+                    String nhaXuatBan = sc.nextLine();
+                    System.out.println("Nhap so ban phat hanh");
+                    int soBanPhatHanh = sc.nextInt();
+                    sc.nextLine();
+                    switch (option) {
+                        case 1:
+                            System.out.println("Nhap ten tac gia");
+                            String tenTacGia = sc.nextLine();
+                            System.out.println("Nhap so trang");
+                            int soTrang = sc.nextInt();
+                            obj = new Sach(MATAILIEU, nhaXuatBan, soBanPhatHanh, tenTacGia, soTrang);
+                            ql.addTaiLieu(obj);
+                            break;
+                        case 2:
+                            System.out.println("Nhap so phat hanh");
+                            int soPhatHanh = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println("Nhap thang phat hanh");
+                            int thangPhatHanh = sc.nextInt();
+                            obj = new TapChi(MATAILIEU, nhaXuatBan, soBanPhatHanh, soPhatHanh, thangPhatHanh);
+                            ql.addTaiLieu(obj);
+                            break;
+                        case 3:
+                            System.out.println("Nhap ngay phat hanh");
+                            String ngayPhatHanh = sc.nextLine();
+                            obj = new Bao(MATAILIEU, nhaXuatBan, soBanPhatHanh, ngayPhatHanh);
+                            ql.addTaiLieu(obj);
+                            break;
+                    }
                     break;
                 case 2:
-                    ql.deleteTailieu();
+                    System.out.println("Nhap ma tai lieu muon xoa");
+                    int maTaiLieu = sc.nextInt();
+                    ql.deleteTailieu(maTaiLieu);
                     break;
                 case 3:
                     ql.displayTaiLieu();
                     break;
                 case 4:
-                    ql.searchTaiLieu();
+                    System.out.println("Ban muon tim loai tai lieu nao ???");
+                    System.out.println("1.Sach  2.Tap chi  3.Bao");
+                    int type = sc.nextInt();
+                    ql.searchTaiLieu(type);
                     break;
                 case 0:
                     System.exit(0);
             }
         }
     }
-    public static void display(){
+
+    public static void display() {
         System.out.println("=============Menu===============");
         System.out.println("1.Them moi tai lieu");
         System.out.println("2.Xoa tai lieu theo MATAILIEU");
--- src/bai2/QuanLySach.java
+++ src/bai2/QuanLySach.java
@@ -1,34 +1,37 @@
 package bai2;
 
-import java.util.Scanner;
 
 public class QuanLySach {
-    private TaiLieu[] danhsach;
+    private final TaiLieu[] danhsach;
     private int index;
 
     public QuanLySach() {
         this.danhsach = new TaiLieu[10];
         this.index = 0;
     }
-    public void deleteTailieu(){
-        Scanner sc = new Scanner(System.in);
+
+    public void addTaiLieu(TaiLieu obj) {
+        danhsach[index] = obj;
+        index++;
+    }
+
+    public void deleteTailieu(int maTaiLieu) {
         int indexOfXoa = -1;
-        System.out.println("Nhap ma tai lieu muon xoa");
-        int maTaiLieu = sc.nextInt();
         for (int i = 0; i < index; i++) {
-            if(maTaiLieu==danhsach[i].getMATAILIEU()){
+            if (maTaiLieu == danhsach[i].getMATAILIEU()) {
                 indexOfXoa = i;
                 break;
             }
         }
-        if(indexOfXoa >= 0) {
+        if (indexOfXoa >= 0) {
             danhsach[indexOfXoa] = null;
             for (int i = indexOfXoa; i < index; i++) {
-                danhsach[i] = danhsach[i+1];
-                danhsach[i+1] = null;
+                danhsach[i] = danhsach[i + 1];
+                danhsach[i + 1] = null;
             }
-            this.index = this.index - 1;
-        }else {
+            System.out.println("Xoa thanh cong 1");
+            index--;
+        } else {
             System.out.println("Khong tim thay ma tai lieu nay");
         }
     }
@@ -39,12 +42,8 @@ public class QuanLySach {
         }
     }
 
-    public void searchTaiLieu() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ban muon tim loai tai lieu nao ???");
-        System.out.println("1.Sach  2.Tap chi  3.Bao");
-        int choice = sc.nextInt();
-        switch (choice) {
+    public void searchTaiLieu(int type) {
+        switch (type) {
             case 1:
                 boolean hasSach = false;
                 for (int i = 0; i < index; i++) {
@@ -84,61 +83,5 @@ public class QuanLySach {
         }
     }
 
-    public void addTaiLieu() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ban muon them loai tai lieu nao ???");
-        System.out.println("1.Sach  2.Tap chi  3.Bao");
-        int option = sc.nextInt();
-        switch (option) {
-            case 1:
-                System.out.println("Nhap ma tai lieu");
-                int MATAILIEU = sc.nextInt();
-                sc.nextLine();
-                System.out.println("Nhap ten nha xuat ban");
-                String nhaXuatBan = sc.nextLine();
-                System.out.println("Nhap so ban phat hanh");
-                int soBanPhatHanh = sc.nextInt();
-                sc.nextLine();
-                System.out.println("Nhap ten tac gia");
-                String tenTacGia = sc.nextLine();
-                System.out.println("Nhap so trang");
-                int soTrang = sc.nextInt();
-                danhsach[index] = new Sach(MATAILIEU, nhaXuatBan, soBanPhatHanh, tenTacGia, soTrang);
-                index++;
-                break;
-            case 2:
-                System.out.println("Nhap ma tai lieu");
-                int MATAILIEU1 = sc.nextInt();
-                sc.nextLine();
-                System.out.println("Nhap ten nha xuat ban");
-                String nhaXuatBan1 = sc.nextLine();
-                System.out.println("Nhap so ban phat hanh");
-                int soBanPhatHanh1 = sc.nextInt();
-                sc.nextLine();
-                System.out.println("Nhap so phat hanh");
-                int soPhatHanh = sc.nextInt();
-                sc.nextLine();
-                System.out.println("Nhap thang phat hanh");
-                int thangPhatHanh = sc.nextInt();
-                danhsach[index] = new TapChi(MATAILIEU1, nhaXuatBan1, soBanPhatHanh1, soPhatHanh, thangPhatHanh);
-                index++;
-                break;
-            case 3:
-                System.out.println("Nhap ma tai lieu");
-                int MATAILIEU2 = sc.nextInt();
-                sc.nextLine();
-                System.out.println("Nhap ten nha xuat ban");
-                String nhaXuatBan2 = sc.nextLine();
-                System.out.println("Nhap so ban phat hanh");
-                int soBanPhatHanh2 = sc.nextInt();
-                sc.nextLine();
-                System.out.println("Nhap ngay phat hanh");
-                String ngayPhatHanh = sc.nextLine();
-                danhsach[index] = new Bao(MATAILIEU2, nhaXuatBan2, soBanPhatHanh2, ngayPhatHanh);
-                index++;
-                break;
-            default:
-                System.out.println("Invalid");
-        }
-    }
+
 }
